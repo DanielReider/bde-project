d9aa24a2084f3657890f0ea615bff8471387fabf
Nico321 <nico.lindmeyer@web.de>
2016-01-27 15:06:41 +0100

    first commit

---
+++ twitchpull/src/main/java/de/fhmuenster/bde/mr/MapClass.java
@@ -0,0 +1,120 @@
+package de.fhmuenster.bde.mr;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+class MapClass extends MapReduceBase implements
+		Mapper<LongWritable, Text, Text, Text> {
+	private Text resKey = new Text();
+	private Text word = new Text();
+
+	public void map(LongWritable key, Text value,
+			OutputCollector<Text, Text> output, Reporter reporter)
+			throws IOException {
+		String line = value.toString();
+		String separator = "\t";
+		Boolean success = false;
+		int trys = 0;
+		StringBuffer response = null;
+
+		while (success == false && trys < 3) {
+			try {
+				Enumeration<NetworkInterface> e = NetworkInterface
+						.getNetworkInterfaces();
+				while (e.hasMoreElements()) {
+					NetworkInterface n = (NetworkInterface) e.nextElement();
+					Enumeration<InetAddress> ee = n.getInetAddresses();
+					while (ee.hasMoreElements()) {
+						InetAddress i = (InetAddress) ee.nextElement();
+						if (i.getHostAddress().toString().equals("10.60.64.45")) {
+							System.out.println("Setting proxy");
+							System.setProperty("http.proxyHost", "10.60.17.102");
+							System.setProperty("http.proxyPort", "8080");
+							System.setProperty("https.proxyHost", "10.60.17.102");
+							System.setProperty("https.proxyPort", "8080");
+						}
+					}
+				}
+				URL url = new URL(line);
+				HttpURLConnection con = (HttpURLConnection) url
+						.openConnection();
+
+				// optional default is GET
+				con.setRequestMethod("GET");
+
+				// add request header
+				con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						con.getInputStream()));
+				String inputLine;
+				response = new StringBuffer();
+
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+				success = true;
+			} catch (Exception e) {
+				System.out.println("catch! next try:"+trys);
+				trys++;
+			}
+		}
+		if (response != null) {
+			JSONObject obj = new JSONObject(response.toString());
+
+			JSONArray arr = obj.getJSONArray("streams");
+			SimpleDateFormat formatter = new SimpleDateFormat(
+					"yyyy-MM-dd HH:mm:ss");
+			String currentDate = formatter.format(new Date());
+			for (int i = 0; i < arr.length(); i++) {
+				String streamId = arr.getJSONObject(i).getBigInteger("_id")
+						.toString();
+				String viewers = arr.getJSONObject(i).getBigInteger("viewers")
+						.toString();
+				JSONObject channel = arr.getJSONObject(i).getJSONObject(
+						"channel");
+				if (channel != null) {
+					String game = channel.get("game").toString();
+					String name = channel.get("name").toString();
+					String status = channel.get("status").toString();
+					String mature = channel.get("mature").toString();
+					Object updated = channel.get("updated_at");
+					Double fps = arr.getJSONObject(i).getDouble("average_fps");
+
+					if (mature == null)
+						mature = "false";
+					if (game != null && !game.equals("null") && name != null
+							&& !name.equals("null") && status != null
+							&& !status.equals("null")) {
+						resKey.set(streamId);
+
+						word.set(viewers + separator + game + separator + name
+								+ separator + status + separator + currentDate
+								+ separator + mature + separator + updated
+								+ separator + fps);
+						output.collect(resKey, word);
+						System.out.println("writing");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ twitchpull/src/main/java/de/fhmuenster/bde/mr/TwitchPull.java
@@ -0,0 +1,118 @@
+package de.fhmuenster.bde.mr;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Enumeration;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class TwitchPull {
+	public void run() throws Exception {
+		JobConf conf = new JobConf(TwitchPull.class);
+		conf.setJobName("twitchPull");
+		String twitchURL = "https://api.twitch.tv/kraken/streams?limit=#limit#&offset=#offset#";
+				
+		Enumeration<NetworkInterface> e = NetworkInterface
+				.getNetworkInterfaces();
+		while (e.hasMoreElements()) {
+			NetworkInterface n = (NetworkInterface) e.nextElement();
+			Enumeration<InetAddress> ee = n.getInetAddresses();
+			while (ee.hasMoreElements()) {
+				InetAddress i = (InetAddress) ee.nextElement();
+				if (i.getHostAddress().toString().equals("10.60.64.45")) {
+					System.out.println("Setting proxy");
+					System.setProperty("http.proxyHost", "10.60.17.102");
+					System.setProperty("http.proxyPort", "8080");
+					System.setProperty("https.proxyHost", "10.60.17.102");
+					System.setProperty("https.proxyPort", "8080");
+				}
+			}
+		}
+
+		try {
+			Path linksPath = new Path(
+					"hdfs://quickstart.cloudera:8020/data/twitch/streammetadata/input/links.txt");
+			FileSystem fs = FileSystem.get(new Configuration());
+			fs.delete(linksPath, true);
+			BufferedWriter br = new BufferedWriter(new OutputStreamWriter(
+					fs.create(linksPath, true)));
+
+			URL url = new URL(twitchURL.replace("#limit#", "1").replace(
+					"#offset#", "0"));
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			JSONObject obj = new JSONObject(response.toString());
+
+			int total = obj.getBigInteger("_total").intValue();
+			int offset = 0;
+			while (total > offset) {
+				br.write(twitchURL.replace("#limit#", "100").replace(
+						"#offset#", "" + offset));
+				br.newLine();
+				total -= 100;
+				offset += 100;
+			}
+
+			br.close();
+
+			conf.setOutputKeyClass(Text.class);
+			conf.setOutputValueClass(Text.class);
+
+			conf.setMapperClass(MapClass.class);
+
+			conf.setReducerClass(ReduceClass.class);
+
+			// conf.setWorkingDirectory(new Path("/temp"));
+
+			FileInputFormat.addInputPath(conf, linksPath);
+			Calendar cal = Calendar.getInstance();
+			FileOutputFormat.setOutputPath(
+					conf,
+					new Path("/data/twitch/streammetadata/processing/"
+							+ cal.get(Calendar.YEAR) + "/"
+							+ cal.get(Calendar.MONTH) + 1 + "/"
+							+ cal.get(Calendar.DAY_OF_MONTH) + "/"
+							+ cal.get(Calendar.HOUR_OF_DAY) + "/"
+							+ cal.get(Calendar.MINUTE)));
+
+			JobClient.runJob(conf);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		TwitchPull dict = new TwitchPull();
+		try {
+			dict.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
